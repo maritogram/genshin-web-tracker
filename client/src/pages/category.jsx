0a6ced95a497e4f71b3fda62d92992723f7c30fe
mario <estrellamariosantos@gmail.com>
2024-01-03 14:14:01 -0400

    search function is now divided by category

--- client/src/pages/category.jsx
+++ client/src/pages/category.jsx
@@ -6,8 +6,10 @@ import {useQuery} from "react-query";
 
 function SearchBar(){
 
-    const navigate= useNavigate()
+     const [searchParams, setSearchParams] = useSearchParams();
+    const search = searchParams.get('s')
 
+    const navigate= useNavigate()
     const [input, setInput] = useState("")
 
      const handleChange = (e) => {
@@ -17,11 +19,13 @@ function SearchBar(){
 
     const handleSubmit = (e) => {
             e.preventDefault()
-        console.log(input)
-        navigate("../category?s=" + input)
+        if(input !== search){
+
+                    navigate("../category?s=" + input)
+        }
+
 
     }
-
 
     return (
         <form className="search" onSubmit={handleSubmit}>
@@ -53,23 +57,11 @@ function SectionCat({param}) {
 }
 
 
-function WrapperLeft({achievements}) {
+function WrapperLeft({data,status}) {
 
-
-    //FIX THIS WHY IT MOUNTS EVERYTIME CLICKED
-    console.log("Mounts ")
-
+    console.log("mounts")
 
     //need to refactor this as it is repeated two times
-    const fetchCategories = async () => {
-        const res = await fetch('/api/catalog')
-        if (!res.ok){
-            throw new Error('Network response was not ok.')
-        }
-        return res.json()
-    }
-
-    const {data,status} = useQuery("categories", fetchCategories)
 
     if(status === 'loading'){
          return (
@@ -102,30 +94,29 @@ function WrapperLeft({achievements}) {
 }
 
 
-function Info({param}) {
+function Info({param, }) {
 
 
     return (
-        <div className="section progress">
-
+        <div className={"section progress"}>
             <img className="left-img" src="/UI_AchievementIcon_1_0.png"/>
-                <p className="information">
-                    <span className="title">{param.name}</span>
-                    <span className="description">{param.description}</span>
-                </p>
-                <img className="primo" src="/5stprimo.webp"/>
-                <div className="completion">
-                    <button className="claim" type="button">Mark</button>
-                </div>
+            <p className="information">
+                <span className="title">{param.name}</span>
+                <span className="description">{param.description}</span>
+            </p>
+            <img className="primo" src="/5stprimo.webp"/>
+            <div className="completion">
+                <button className="claim" type="button">Mark</button>
             </div>
+        </div>
     )
 }
 
-function WrapperRight() {
+function WrapperRight({categories}) {
 
     const fetchAchievements = async () => {
         const res = await fetch('/api/achievements')
-        if(!res.ok){
+        if (!res.ok) {
             throw new Error('Network response was not ok.')
         }
         return res.json()
@@ -137,14 +128,11 @@ function WrapperRight() {
     const search = searchParams.get('s')
 
 
-
-
-
     const {data, status} = useQuery('achievements', fetchAchievements)
 
-    if(status === 'loading'){
-         return (
-        <div id="wrapper-right">
+    if (status === 'loading') {
+        return (
+            <div id="wrapper-right">
             <div className="canvas">
             </div>
             <div className="paper">
@@ -170,17 +158,26 @@ function WrapperRight() {
     }
 
 
+
+    let i = 0;
+
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-    //I don't think this might be that efficient, but it's better than have a lot of people doing multiple api calls considering each search is one api call.
-    const filtered = data.filter(({category_id, name})=>
-    {
+    //I don't think this might be that efficient, but it's better than having a lot of people do multiple api calls considering each search is one api call.
+    const filtered = data.filter(({category_id, name})=> {
         if (categoryId !== undefined){
-           return parseInt(categoryId) === (category_id)
+            return parseInt(categoryId) === (category_id)
         }else{
-            return (name.toLowerCase().includes(search.toLowerCase()))
+
+            const tf = name.toLowerCase().includes(search.toLowerCase())
+            if (tf) i++
+
+            return (tf)
+
         }
-    }
-    )
+    })
+
+    // string that will store the title of the achievement just returned in the map method below.
+    let before = ""
 
 
     return (
@@ -189,9 +186,26 @@ function WrapperRight() {
             </div>
             <div className="paper">
                 <div className="scroll-style ">
-                    {filtered.map((ach)=>
-                        <Info key={ach.ach_id+0} param={ach}></Info>
-                    )}
+                    {filtered.map((ach)=> {
+
+                        const sTitle = categories[ach.category_id - 1].title
+
+
+                        if((categoryId === undefined) && (before !== sTitle)){
+                            before = sTitle
+                            return(
+                                <>
+                                    <div className={"section s-title"} >{sTitle}</div>
+                                    <Info key={ach.ach_id+0} param={ach}></Info>
+                                </>
+                            )
+                        } else {
+                            return (<Info key={ach.ach_id+0} param={ach}></Info>)
+
+                        }
+
+
+                    })}
                 </div>
             </div>
         </div>
@@ -200,10 +214,20 @@ function WrapperRight() {
 
 function Category() {
 
+    const fetchCategories = async () => {
+        const res = await fetch('/api/catalog')
+        if (!res.ok){
+            throw new Error('Network response was not ok.')
+        }
+        return res.json()
+    }
+
+    const {data,status} = useQuery("categories", fetchCategories)
+
     return (
         <div id="content">
-            <WrapperLeft></WrapperLeft>
-            <WrapperRight></WrapperRight>
+            <WrapperLeft data={data} status={status}></WrapperLeft>
+            <WrapperRight categories={data}></WrapperRight>
         </div>
     );
 }
